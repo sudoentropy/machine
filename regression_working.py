46fd84f5bbd3b8c4c49018e3007df9e70575635f
epower <entropy.omega@gmail.com>
2019-12-20 21:47:02 -0500

    selected best model

--- regression_working.py
+++ regression_working.py
@@ -26,19 +26,16 @@ y = np.array(data[predict])
 #splitting into training and 10% testing data
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
 
-
+"""
 #automating the process to train a mode with high accuracy with a for loop
 best = 0
-for _ in range(100000):
+for _ in range(1000):
 
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
     #fitting data to a linear model
     linear = linear_model.LinearRegression()
 
-    """section commented out that trains the model
-    #and dumps it into pickle juice
-    no longer needed as model was trained and saved"""
     #create a model and test its accuracy
     linear.fit(x_train, y_train)
     accuracy = linear.score(x_test, y_test)
@@ -51,7 +48,7 @@ for _ in range(100000):
         #section commented out that saves the model
         #saving trained model
         with open("grade_pred_model.pickle", "wb") as f:
-            pickle.dump(linear, f)
+            pickle.dump(linear, f)"""
 
 #defining variable.....i believe, probably incorrect prog term but whatevs
 pickle_in = open("grade_pred_model.pickle", "rb")
