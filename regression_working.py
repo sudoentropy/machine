154bf4ebe33efb4e8da4453d058d054be7d5175c
epower <entropy.omega@gmail.com>
2019-12-21 17:58:26 -0500

    modified venv to optimize multicore

--- regression_working.py
+++ regression_working.py
@@ -6,27 +6,6 @@ from sklearn.utils import shuffle
 import matplotlib.pyplot as pyplot
 import pickle #pickle is used to save trained model
 from matplotlib import style
-"""
-#this shit is multi core optimization, does not work as coded
-#from keras import backend as K
-import tensorflow as tf
-
-NUM_PARALLELL_EXEC_UNITS = 16
-
-config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLELL_EXEC_UNITS, inter_op_parallelism_threads=2, allow_soft_placement=True, device_count={'CPU': NUM_PARALLELL_EXEC_UNITS})
-
-session = tf.Session(config=config)
-
-#K.set_session(session)
-
-os.environ["OMP_NUM_THREADS"] = "16"
-
-os.environ["KMP_BLOCKTIME"] = "30"
-
-os.environ["KMP_SETTINGS"] = "1"
-
-os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact1,0"
-#end multicore opt"""
 
 #read data into pandas dataframe, sep is listing delinieator
 data = pd.read_csv("student-mat.csv", sep=";")
