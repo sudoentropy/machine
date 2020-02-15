65390ce7a9eeaa34f131bdcb54421694835fc804
epower <entropy.omega@gmail.com>
2020-02-14 22:39:34 -0500

    linear model

--- lin_fit.py
+++ lin_fit.py
@@ -7,10 +7,14 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+
+
+# importiung data, linear regression, printig accuracy and coefficients
+# manually testing test data and printing to terminal
 
 data = pd.read_csv("student-mat.csv", sep=";")
-
 data = data[["G1", "G2", "G3", "studytime", "failures",
              "absences"]]
 
@@ -19,5 +23,21 @@ predict = "G3"
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, y_train, x_test, y_test, = sklearn.model_selection.train_test_split(
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
     X, y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+
+print(acc)
+print("Co: ", linear.coef_)
+print("Intercept: ", linear.intercept_)
+
+pred = linear.predict(x_test)
+
+for x in range(len(pred)):
+    print(pred[x], x_test[x], y_test[x])
+
+
+# using generated model
