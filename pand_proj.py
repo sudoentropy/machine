483473e3f3b1650873eb7c2706c7bb6340d9426b
entropy omega <entropy.omega@gmail.com>
2020-02-25 22:09:32 -0500

    pandas learning

--- pand_proj.py
+++ pand_proj.py
@@ -1,11 +1,19 @@
+"""
+taking lead of gene researchers
+dumping most of the data to find important unique patterns
+Assumption: probably most of it is false patterns
+"""
+
+
 import pandas as pd
 
 
-brain = pd.read_csv('Brain_GSE5.csv')
-# print(brain.head())
-# print(brain.tail())
-# print(brain)
-# print(brain.columns)
+df = pd.read_csv('Brain_GSE5.csv')
 
-# loading csv into dataframe
+# transposing the data because it has 50,000 columns of genes
+# maybe the genes are the data, not the features???
+df_t = df.T
+
+
+
 
