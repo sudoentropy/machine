3b688fc3bf7694600059380180c5fa351099bad1
entropy omega <entropy.omega@gmail.com>
2020-02-28 20:43:12 -0500

    sqlalchemy

--- pand_proj.py
+++ pand_proj.py
@@ -8,3 +8,8 @@ Assumption: probably most of it is false patterns
 import pandas as pd
 import sqlalchemy
 
+# connecting to local sql database
+engine = sqlalchemy.create_engine('mysql+pymysql://root:@127.0.0.1:1433/csv_test')
+
+df = pd.read_sql_table("genes", engine)
+print(df)
