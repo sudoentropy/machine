998949b3d106bf0931a45e9cebd064f1c784ed31
entropy omega <entropy.omega@gmail.com>
2020-02-28 21:18:31 -0500

    functional sql server connection

--- pand_proj.py
+++ pand_proj.py
@@ -7,7 +7,7 @@ import pandas as pd
 import sqlalchemy
 from sqlalchemy import create_engine
 
-# connecting to local sql database
+# connecting to local sql database, this code below works 2-28-20
 SERVER = 'ETHINK'
 DATABASE = 'csv_test'
 DRIVER = 'SQL Server Native Client 11.0'
@@ -18,5 +18,5 @@ DATABASE_CONNECTION = f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver
 engine = create_engine(DATABASE_CONNECTION)
 connection = engine.connect()
 
-df = pd.read_sql_table("genes", engine)
+df = pd.read_sql_query("select top 1 * from [csv_test].[dbo].[human_liver]", connection)
 print(df)
