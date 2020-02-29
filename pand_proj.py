b352ad778ba5bb8558029d3ba051b366a1004e41
entropy omega <entropy.omega@gmail.com>
2020-02-29 13:16:37 -0500

    handling columns

--- pand_proj.py
+++ pand_proj.py
@@ -1,5 +1,11 @@
 """
 connecting sql alchemy to local sqlserver
+
+
+have learned how to connect to sql server
+use sqlalchemy to talk to server
+some pyodb i guess because certain method would not work without it
+also have learned to reference select columns
 """
 
 
@@ -19,7 +25,7 @@ DATABASE_CONNECTION = f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver
 engine = create_engine(DATABASE_CONNECTION)
 connection = engine.connect()
 
-# essentially a head test
+# essentially a head test, with sqly language and shiiiite
 # df = pd.read_sql_query(
 #     "select top 10 * from [csv_test].[dbo].[human_liver]",
 #     connection)
@@ -30,8 +36,11 @@ connection = engine.connect()
 # print(df.head())
 
 # test with "connection" method
-df= pd.read_sql_table("human_liver", connection)
-print(df.head())
-
 # connection is probably more correct way to do this shiiiit
 
+# this is referencing only the column genes
+# df= pd.read_sql_table("human_liver", connection, columns=['genes'])
+# print(df.head())
+
+df = pd.read_sql_table("human_liver", connection, columns=['genes', 'GSM2055788'])
+print(df.head())
