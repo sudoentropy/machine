ef00b8c5b3073685bc09dcca03df1a7f5922e721
entropy omega <entropy.omega@gmail.com>
2020-02-28 20:11:26 -0500

    sqlalchemy

--- pand_proj.py
+++ pand_proj.py
@@ -6,16 +6,5 @@ Assumption: probably most of it is false patterns
 
 
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-
-df = pd.read_csv('Brain_GSE5.csv')
-
-# transposing the data because it has 50,000 columns of genes
-# maybe the genes are the data, not the features???
-df_t = df.T
-
-# testing functionality
-# print(df_t.head())
-
-# label encoder step
+import sqlalchemy
 
